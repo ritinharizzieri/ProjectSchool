0bc710ba40d46af95a0c8da8bab5941eb7583f27
Rita Rizzieri <rita.rizzieri@checklistfacil.com>
2023-06-20 08:53:39 -0300

    limpando material module deixando apenas o que precisa no momento

--- src/app/shared/materials/material.module.ts
+++ src/app/shared/materials/material.module.ts
@@ -4,20 +4,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatInputModule } from '@angular/material/input';
-import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 import { MatListModule } from '@angular/material/list';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatNativeDateModule } from '@angular/material/core'
-import { MAT_DATE_LOCALE } from '@angular/material/core'
-import { MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarModule } from '@angular/material/snack-bar'
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [],
@@ -26,37 +13,9 @@ import { MatSortModule } from '@angular/material/sort';
     MatIconModule,
     MatButtonModule,
     MatSidenavModule,
-    MatInputModule,
     MatListModule,
-    MatProgressBarModule,
-    MatProgressSpinnerModule,
-    MatTabsModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatSnackBarModule,
-    MatDialogModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule
   ],
   // configuração esta global, dessa forma todos as props do mat-form-field seguem o mesmo padrão
-  providers:[
-    {
-      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
-      useValue:{ appearance: 'outline', floatLabel: 'always'}
-    },
-    {
-      provide: MAT_DATE_LOCALE,
-      useValue: 'pt-br'
-    },
-    {
-      provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
-      useValue: {
-        duration: 2500,
-        horizontalPosition: 'end',
-        verticalPosition: 'top'
-      }
-    }
-  ]
+  providers:[]
 })
 export class MaterialModule { }
