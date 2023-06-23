bef6ace3409d59cba856440509db5e7a8c9a92de
Rita Rizzieri <rita.rizzieri@checklistfacil.com>
2023-06-23 19:28:31 -0300

    cria modules teachers, students e support

---
+++ src/app/shared/materials/teachers-mat.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@NgModule({
+  declarations: [],
+  exports:[
+    MatIconModule,
+    MatButtonModule
+  ],
+  providers:[]
+})
+export class TeachersMaterialModule { }
