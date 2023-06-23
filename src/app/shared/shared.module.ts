2d5c69f7ce7e1696c82759e951971aa8079d93f5
Rita Rizzieri <rita.rizzieri@checklistfacil.com>
2023-06-23 08:51:09 -0300

    estilizando o component title

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { ToolbarTitleComponent } from './components/toolbar-title/toolbar-title.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatIconModule
+  ],
   declarations: [
     ToolbarTitleComponent
   ],
