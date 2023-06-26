02dac6a31f3f54a98e6d10c3daa7111a9df69cbe
Rita Rizzieri <rita.rizzieri@checklistfacil.com>
2023-06-26 20:57:55 -0300

    adicionando foto de perfil e estilizando com css menu lateral

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,7 @@ export const SHADOW_LIMIT = 100;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit  {
   public isSmallScreen:boolean = false;
